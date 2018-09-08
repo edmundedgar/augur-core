e7e1d42595c81c6c3e0ebb62cad2e37f3bee1caa
Edmund Edgar <ed@realitykeys.com>
2018-09-08 00:12:04 +0000

    Test tidying, assign different accounts names

--- tests/test_realitio.py
+++ tests/test_realitio.py
@@ -36,16 +36,25 @@ def test_realitio_invalid_last_incorrect_true(localFixture, controller, universe
 def test_realitio_invalid_last_incorrect_false(localFixture, controller, universe):
 	realitiocon = _test_answer_set(localFixture, controller, universe, REALITIO_NO, REALITIO_INVALID, False)
 
-	#realitiocon = _test_answer_set(localFixture, controller, universe, longTo32Bytes(long(1)), longTo32Bytes(long(0)), False)
-	#realitiocon = _test_answer_set(localFixture, controller, universe, longTo32Bytes(long(1)), longTo32Bytes(long(0)), False)
-	#realitiocon = _test_answer_set(localFixture, controller, universe, longTo32Bytes(long(1)), longTo32Bytes(long(0)), True)
-	#realitiocon = _test_answer_set(localFixture, controller, universe, longTo32Bytes(long(1)), longTo32Bytes(long(0)), None)
-	
 def _test_answer_set(localFixture, controller, universe, realitio_answer_final, realitio_answer_wrong, augur_bool):
 
     augarb = controller.lookup("AugurArbitrator")
     realitio = controller.lookup("Realitio")
     cash = controller.lookup("Cash")
+
+    (a_rep_faucet, k_rep_faucet) = (tester.a0, tester.k0)
+
+    (a_asker, k_asker) = (tester.a1, tester.k1)
+
+    (a_market_owner, k_market_owner) = (tester.a2, tester.k2)
+
+    (a_answerer1, k_answerer1) = (tester.a3, tester.k3)
+    (a_answerer2, k_answerer2) = (tester.a4, tester.k4)
+
+    (a_reporter, k_reporter) = (tester.a5, tester.k5)
+
+    (a_arb_requester, k_arb_requester) = (tester.a8, tester.k8)
+
 
     # Params used in the Realitio question
     template_id = 0 # This is our basic yes/no question type
@@ -63,8 +72,7 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
 
     realitiocon = localFixture.uploadAndAddToController("../source/contracts/Realitio.sol", lookupKey="Realitio")
 
-    question_asker = tester.a1
-    question_id = realitiocon.askQuestion(template_id, "Is this thing on?", augurarbcon.address, timeout, opening_ts, nonce, sender=tester.k1)
+    question_id = realitiocon.askQuestion(template_id, "Is this thing on?", augurarbcon.address, timeout, opening_ts, nonce, sender=k_asker)
 
     answer_hist_hash = []
     answer_hist_addr = []
@@ -72,22 +80,22 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
     answer_hist_answ = []
 
     answer_hist_hash.append(realitiocon.questions(question_id)[8])
-    realitiocon.submitAnswer(question_id, realitio_answer_final, 0, sender=tester.k3, value=321)
-    answer_hist_addr.append(tester.a3)
+    realitiocon.submitAnswer(question_id, realitio_answer_final, 0, sender=k_answerer1, value=321)
+    answer_hist_addr.append(a_answerer1)
     answer_hist_bond.append(321)
     answer_hist_answ.append(realitio_answer_final)
 
     answer_hist_hash.append(realitiocon.questions(question_id)[8])
 
-    realitiocon.submitAnswer(question_id, realitio_answer_wrong, 0, sender=tester.k4, value=642)
+    realitiocon.submitAnswer(question_id, realitio_answer_wrong, 0, sender=k_answerer2, value=642)
 
-    answer_hist_addr.append(tester.a4)
+    answer_hist_addr.append(a_answerer2)
     answer_hist_bond.append(642)
     answer_hist_answ.append(realitio_answer_wrong)
 
     answer_hist_hash.append(realitiocon.questions(question_id)[8])
-    realitiocon.submitAnswer(question_id, realitio_answer_final, 0, sender=tester.k3, value=1400)
-    answer_hist_addr.append(tester.a3)
+    realitiocon.submitAnswer(question_id, realitio_answer_final, 0, sender=k_answerer1, value=1400)
+    answer_hist_addr.append(a_answerer1)
     answer_hist_bond.append(1400)
     answer_hist_answ.append(realitio_answer_final)
 
@@ -101,41 +109,38 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
  
     repaddr = universe.getReputationToken()
     rep = localFixture.applySignature('ReputationToken', repaddr)
-    t0rep = rep.balanceOf(tester.a0)
-    assert t0rep > 0
+    assert rep.balanceOf(a_rep_faucet) > 0
 
-    # We'll make ourselves the designated reporter. Somebody else would work too though.
-    designated_reporter = tester.a1
 
     # Revert if the max_previous is too low
     with raises(TransactionFailed):
-        augurarbcon.requestArbitration(question_id, 320, value=12345, sender=tester.k8)
+        augurarbcon.requestArbitration(question_id, 320, value=12345, sender=k_arb_requester)
 
     # Revert if you haven't yet requested arbitration
     with raises(TransactionFailed):
-        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, question_asker, nonce, designated_reporter)
+        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, a_asker, nonce, a_reporter, value=valbond)
 
-    augurarbcon.requestArbitration(question_id, 0, value=12345, sender=tester.k8)
+    augurarbcon.requestArbitration(question_id, 0, value=12345, sender=k_arb_requester)
 
     # Fail if the contract doesn't yet own sufficient REP
     with raises(TransactionFailed):
-        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, question_asker, nonce, designated_reporter, value=valbond)
+        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, a_asker, nonce, a_reporter, value=valbond)
 
     # Make sure the arbitrator contract has some REP for the validity bond
     # TODO: Work out how to prevent someone else from spending the REP before you can use it
     # Either use an intermediate contract owned per-user or do something clever with transferFrom
-    rep.transfer(augurarbcon.address, noshowbond, sender=tester.k0)
+    rep.transfer(augurarbcon.address, noshowbond, sender=k_rep_faucet)
     assert rep.balanceOf(augurarbcon.address) == noshowbond 
 
     # This will do some checks then call:
-    # IMarket market = universe.createYesNoMarket.value(msg.value)( now+1, 0, market_token, designated_reporter, 0x0, question, "");
+    # IMarket market = universe.createYesNoMarket.value(msg.value)( now+1, 0, market_token, a_reporter, 0x0, question, "");
 
-    augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, question_asker, nonce, designated_reporter, value=valbond)
+    augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, a_asker, nonce, a_reporter, value=valbond)
 
     # You can only do this once per question
-    rep.transfer(augurarbcon.address, noshowbond, sender=tester.k0)
+    rep.transfer(augurarbcon.address, noshowbond, sender=k_rep_faucet)
     with raises(TransactionFailed):
-        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, question_asker, nonce, designated_reporter, value=valbond)
+        augurarbcon.createMarket("Is this thing on?", timeout, opening_ts, a_asker, nonce, a_reporter, value=valbond)
 
     market_addr = augurarbcon.realitio_questions(question_id)[2]
     owner_addr = augurarbcon.realitio_questions(question_id)[3]
@@ -149,17 +154,17 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
     proceedToDesignatedReporting(localFixture, market)
 
     reporter_stake = universe.getOrCacheDesignatedReportStake()
-    rep.transfer(designated_reporter, reporter_stake, sender=tester.k0)
+    rep.transfer(a_reporter, reporter_stake, sender=k_rep_faucet)
 
     augur_answer_in_realitio = None
     if augur_bool is None:
-        market.doInitialReport([0, 0], True, sender=tester.k1)
+        market.doInitialReport([0, 0], True, sender=k_reporter)
         augur_answer_realitio_hex = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
     elif augur_bool:
-        market.doInitialReport([0, market.getNumTicks()], False, sender=tester.k1)
+        market.doInitialReport([0, market.getNumTicks()], False, sender=k_reporter)
         augur_answer_realitio_hex = "0x0000000000000000000000000000000000000000000000000000000000000001";
     else:
-        market.doInitialReport([market.getNumTicks(), 0], False, sender=tester.k1)
+        market.doInitialReport([market.getNumTicks(), 0], False, sender=k_reporter)
         augur_answer_realitio_hex = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
     # the market is now assigned a fee window
@@ -173,31 +178,34 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
 
     hash_before_arb_report = realitiocon.questions(question_id)[8]
 
-    with raises(TransactionFailed, message="answer mismatch should fail"):
-        augurarbcon.reportAnswer(question_id, answer_hist_hash[-1], realitio_answer_final, 1400, tester.a5, False)
+    with raises(TransactionFailed, message="answerer mismatch should fail"):
+        augurarbcon.reportAnswer(question_id, answer_hist_hash[-1], realitio_answer_final, 1400, tester.a9, False)
   
     assert not realitiocon.isFinalized(question_id)
 
-    augurarbcon.reportAnswer(question_id, answer_hist_hash[-1], realitio_answer_final, 1400, tester.a3, False)
+    augurarbcon.reportAnswer(question_id, answer_hist_hash[-1], realitio_answer_final, 1400, a_answerer1, False)
 
     assert realitiocon.isFinalized(question_id)
     assert hash_before_arb_report != realitiocon.questions(question_id)[8]
+
+    with raises(TransactionFailed, message="You can only report an answer once"):
+        augurarbcon.reportAnswer(question_id, answer_hist_hash[-1], realitio_answer_final, 1400, a_answerer1, False)
 
     ans = realitiocon.getFinalAnswer(question_id)
     assert bytes32ToHexString(ans) == augur_answer_realitio_hex
 
     claimer = None
     if (bytes32ToHexString(realitio_answer_final) == augur_answer_realitio_hex):
-        claimer = tester.a3
+        claimer = a_answerer1
     else:
-        claimer = tester.a8
+        claimer = a_arb_requester
 
     answer_hist_hash.append(hash_before_arb_report)
     answer_hist_addr.append(claimer)
     answer_hist_bond.append(0)
     answer_hist_answ.append(ans)
 
-    start_bal = realitiocon.balanceOf(claimer)
+    claimer_start_bal = realitiocon.balanceOf(claimer)
     realitiocon.claimWinnings(
         question_id, 
         answer_hist_hash[::-1],
@@ -205,7 +213,9 @@ def _test_answer_set(localFixture, controller, universe, realitio_answer_final, 
         answer_hist_bond[::-1],
         answer_hist_answ[::-1]
     )
-    end_bal = realitiocon.balanceOf(claimer)
+    claimer_end_bal = realitiocon.balanceOf(claimer)
+
+    assert (claimer_end_bal - claimer_start_bal) > 0
 
     return realitiocon
 
